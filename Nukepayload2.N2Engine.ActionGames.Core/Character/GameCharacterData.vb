2d3c7fb1f72c48eac2a7cd4e897fbccd4c4e11e4
experdot <1143116306@qq.com>
2016-09-04 15:52:49 +0800

    添加基础游戏数据模型

---
+++ Nukepayload2.N2Engine.ActionGames.Core/Character/GameCharacterData.vb
@@ -0,0 +1,6 @@
+﻿''' <summary>
+''' 典型的ACT的玩家数据
+''' </summary>
+Public Class GameCharacterData
+
+End Class
---
+++ Nukepayload2.N2Engine.ActionGames.Core/Mission/MissionData.vb
@@ -0,0 +1,6 @@
+﻿''' <summary>
+''' 表示一个关卡数据
+''' </summary>
+Public Class MissionData
+
+End Class
