1c92fd2d5a0fd208960a736199b8d09810ae9980
Nukepayload2 <1939357182@qq.com>
2016-11-01 13:34:52 +0800

    基本的贴图功能 #13

--- N2Demo.Core/ResourceRouteConfig.vb
+++ N2Demo.Core/ResourceRouteConfig.vb
@@ -2,6 +2,9 @@
 Imports System.Reflection
 
 Module ResourceRouteConfig
+    ''' <summary>
+    ''' 注册此程序集嵌入的资源
+    ''' </summary>
     Sub ApplyRoute()
         Dim resldr = ResourceLoader.GetForCurrentView
         resldr.AddRoute("Images", GetType(ResourceRouteConfig).GetTypeInfo.Assembly)
--- N2Demo.Core/SparksView.vb
+++ N2Demo.Core/SparksView.vb
@@ -1,5 +1,4 @@
 ﻿Imports System.Numerics
-Imports Nukepayload2.N2Engine.Platform
 Imports Nukepayload2.N2Engine.Resources
 Imports Nukepayload2.N2Engine.UI.Elements
 Imports Nukepayload2.N2Engine.UI.Views
@@ -37,7 +36,7 @@ Public Class SparksView
         AddVisual(charaSheet)
     End Sub
     ''' <summary>
-    ''' 平台特定实现点击此视图时，调用此方法
+    ''' 平台特定实现点击此视图时，调用此方法。通用的输入事件完成后，此方法将过时。
     ''' </summary>
     Public Sub OnTapped(pos As Vector2)
         sparksData.SparkSys.Location = pos
