77361b6ab4300c0f939e3d23125a2248db923ea3
Nukepayload2 <1939357182@qq.com>
2016-12-04 19:54:42 +0800

    基本事件的基础数据 #31

---
+++ Nukepayload2.N2Engine.Core/Input/GameKeyboardEventArgs.vb
@@ -0,0 +1,17 @@
+﻿Namespace Input
+    ''' <summary>
+    ''' 为键盘事件提供数据
+    ''' </summary>
+    Public Class GameKeyboardEventArgs
+        Inherits EventArgs
+
+        Sub New(keyCode As Key)
+            Me.KeyCode = keyCode
+        End Sub
+
+        ''' <summary>
+        ''' 代表哪个按键与这个事件相关
+        ''' </summary>
+        Public ReadOnly Property KeyCode As Key
+    End Class
+End Namespace
