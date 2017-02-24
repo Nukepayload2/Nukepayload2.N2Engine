f23fa5e6a98460a0a97235f467cffe5bafdcf628
Nukepayload2 <1939357182@qq.com>
2017-02-24 22:12:39 +0800

    将许可文件设置为生成时较新则复制到输出目录

--- Nukepayload2.N2Engine.Core/UI/Elements/ParticleSystemViews/ParticleSystemView.vb
+++ Nukepayload2.N2Engine.Core/UI/Elements/ParticleSystemViews/ParticleSystemView.vb
@@ -9,5 +9,16 @@ Namespace UI.ParticleSystemViews
 
         Public ReadOnly Property Data As New PropertyBinder(Of T)
 
+        Public Overrides Property UpdateAction As Action
+            Get
+                Return Sub()
+                           MyBase.UpdateAction.Invoke()
+                           Data.Value.Update()
+                       End Sub
+            End Get
+            Set(value As Action)
+                MyBase.UpdateAction = value
+            End Set
+        End Property
     End Class
 End Namespace
--- Nukepayload2.N2Engine.Core/UI/Elements/ParticleSystemViews/SparkParticleSystemView.vb
+++ Nukepayload2.N2Engine.Core/UI/Elements/ParticleSystemViews/SparkParticleSystemView.vb
@@ -4,16 +4,6 @@ Namespace UI.ParticleSystemViews
 
     Public Class SparkParticleSystemView
         Inherits ParticleSystemView(Of SparkParticleSystem)
-        Public Overrides Property UpdateAction As Action
-            Get
-                Return Sub()
-                           MyBase.UpdateAction.Invoke()
-                           Data.Value.Update()
-                       End Sub
-            End Get
-            Set(value As Action)
-                MyBase.UpdateAction = value
-            End Set
-        End Property
+
     End Class
 End Namespace
